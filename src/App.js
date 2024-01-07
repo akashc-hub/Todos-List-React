143302f357db89b5e62ea7bcb3648de98cb41178
Akash <akashchauhan84590@gmail.com>
2024-01-07 12:29:09 +0530

    New Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Header from './MyComponents/Header'
+import Todos from './MyComponents/Todos'
+import Footer from './MyComponents/Footer'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const onDelete = (todo) =>{
+    console.log("I am on Delete of todo", todo)
 
+  }
+
+  let todos = [
+    {
+      sno : 1,
+      title : "Go to the Market",
+      desc : "You need to go and Complete the Job1"
+    },
+    {
+      sno : 2,
+      title : "Go to the Mall",
+      desc : "You need to go and Complete the Job2"
+    },
+    {
+    sno : 3,
+      title : "Go to the Shopping",
+      desc : "You need to go and Complete the Job3"
+    }
+  ]
+  return (
+   <>
+    <Header title="My Todo List" searchBar={false}/>
+    <Todos todos= {todos} onDelete={onDelete}/>
+    <Footer/>
+   </>
+  );
+
+  }
 export default App;
