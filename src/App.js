4472272272f11759c7710a69db8dd299df556dfd
Akash <akashchauhan84590@gmail.com>
2024-01-09 10:55:43 +0530

    New Commit

--- src/App.js
+++ src/App.js
@@ -3,47 +3,80 @@ import Header from './MyComponents/Header';
 import Todos from './MyComponents/Todos';
 import Footer from './MyComponents/Footer';
 import AddTodo from './MyComponents/AddTodo'
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+// import About from './MyComponents/About'
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route,
+//   Link
+// } from "react-router-dom";
 
 function App() {
-  const onDelete = (todo) =>{
+  let initTodo;
+  if (localStorage.getItem("todos") === null) {
+    initTodo = [];
+  }
+  else {
+    initTodo = JSON.parse(localStorage.getItem("todos"))
+  }
+  const onDelete = (todo) => {
     console.log("I am on Delete of todo", todo);
     // Deleting this way is not works in react
     // let index = todos.indexOf(todo);
     // todos.splice(index, 1);
 
-    setTodos(todos.filter((e)=>{
-      return e!==todo;
-    }))
+    setTodos(todos.filter((e) => {
+      return e !== todo;
+    }));
+    localStorage.setItem("todos", JSON.stringify(todos));
   }
 
-  const [todos, setTodos] = useState( [
-    {
-      sno : 1,
-      title : "Go to the Market",
-      desc : "You need to go and Complete the Job1"
-    },
-    {
-      sno : 2,
-      title : "Go to the Mall",
-      desc : "You need to go and Complete the Job2"
-    },
-    {
-    sno : 3,
-      title : "Go to the Shopping",
-      desc : "You need to go and Complete the Job3"
+  const addTodo = (title, desc) => {
+    console.log("I am adding this todo", title)
+    let sno;
+    if (todos.length === 0) {
+      sno = 0;
     }
-  ]);
+    else {
+      sno = todos[todos.length - 1].sno + 1;
+    }
+    const myTodo = {
+      sno: sno,
+      title: title,
+      desc: desc
+    }
+    setTodos([...todos, myTodo]);
+    console.log(myTodo)
+
+  }
+
+  const [todos, setTodos] = useState(initTodo);
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos])
 
   return (
-   <>
-    <Header title="My Todo List" searchBar={false}/>
-    <AddTodo/>
-    <Todos todos= {todos} onDelete={onDelete}/>
-    <Footer/>
-    
-   </>
+    <>
+      {/* <Router> */}
+        <Header title="My Todo List" searchBar={false} />
+        {/* <Switch>
+          <Route path="/" render={() => {
+            return (
+              <> */}
+                <AddTodo addTodo={addTodo} />
+                <Todos todos={todos} onDelete={onDelete} />
+              {/* </>)
+          }}>
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+        </Switch> */}
+        <Footer />
+      {/* </Router> */}
+    </>
   );
 
-  }
+}
 export default App;
---
+++ src/MyComponents/About.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+        This is an About Component
+        <p>
+        Step 1: Know your goals
+
+Your to-do list will contain the tasks you’d like to accomplish each day. Ideally, these should correlate with the milestones you’re aiming to achieve each month or quarter.
+
+Step 2: Break down your list
+
+Once you know where you’re heading, break down those bigger projects into smaller tasks that’ll mark your progress along the way. For example, “write essay for class” might turn into “research essay”, then “outline essay” and so on.
+
+This will help you build momentum and chart your progress.
+
+Step 3: Choose controllable tasks
+
+If your to-do list consists of tasks that require other people’s input to complete them, there’s a chance you’ll never get them done. Instead, look at what you can do to increase your chances of success.
+
+For instance, “get presentation from Julie” might become “chase Julie for presentation”. That way, you can see the actions that you’re taking.
+
+Step 4: Prioritise your tasks
+
+Even those of us who write our to-do list with the best of intentions don’t always achieve everything we’d hoped for each day. But we can make some small changes to improve our chances.
+
+Place your most important tasks at the top of your to-do list. That way, you’ll come to these tasks first when you’re likely to have more energy and more motivation. The lower priority tasks can sit at the bottom of your list, to be tackled if time and space allows.
+
+Step 5: Make it manageable
+
+Try to be realistic about what you can accomplish in a single day. If you never complete your to-do list, your motivation is going to dwindle.
+
+If you’re finding that you're constantly missing out on your tasks, go the other way. Write a to-do list that you’re 100% sure you can achieve. And then build up your tasks from there, bit by bit.
+        </p>
+    </div>
+  )
+}
+
+export default About
